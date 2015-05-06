42babdabbfff7d9ccd2b62609750b8bcb22e490a
Wilson Buntu <pi@piscuit.(none)>
2015-05-06 21:10:46 +0000

    some more refacto. New object Plug

--- pi-plug.js
+++ pi-plug.js
@@ -1,14 +1,34 @@
 var exec = require('child_process').exec;
 var util = require('util');
 
-var switchIt = function(plugLetter, switchChoice) {
-        console.log(switchChoice);
-        var plugNumber = 1;
-        if ('d' === plugLetter) plugNumber = 4;
-        var switchCommand = 0;
-        if ('on' === switchChoice) switchCommand = 1;
+var Plug = function(letter, choice) {
+  this.code = '11111';
+  this.letter = letter;
+  this.choice = choice;
 
-        exec(util.format('sudo /home/pi/tools_installed/433Utils/RPi_utils/send 11111 %d %d', plugNumber, switchCommand), {encoding: 'utf8'}, function(err, stdout) {
+  this.getPlugNumber = function () {
+    var plugNumber = 1;
+    if ('d' === this.letter) plugNumber = 4;
+    return plugNumber;
+  }
+
+  this.getSwitchCommand = function () {
+    var switchCommand = 0;
+    if ('on' === this.choice) switchCommand = 1;
+    return switchCommand;
+  }
+
+  this.toString = function () {
+    return util.format('Plug %s, switch it %s', this.letter, this.choice);
+  }
+
+};
+
+var switchIt = function(plugLetter, switchChoice) {
+	var plug = new Plug(plugLetter, switchChoice);
+	console.log(plug.toString());
+
+        exec(util.format('sudo /home/pi/tools_installed/433Utils/RPi_utils/send %s %d %d', plug.code, plug.getPlugNumber(), plug.getSwitchCommand()), {encoding: 'utf8'}, function(err, stdout) {
                 if (err) throw err;
 
                 console.log(stdout);
